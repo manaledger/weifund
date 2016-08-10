f66a6b59e2de6e7a5cf2cd8279a85c3276812fb0
xuxinglai <xuxinglai@mana.com>
2016-08-10 16:30:25 +0800

    Use 360cdn instead of google web site
    unfortunate it can't access google from China, so use 360cdn instead
    of it.

--- app/client/lib/compatibility/lato.js
+++ app/client/lib/compatibility/lato.js
@@ -4,7 +4,7 @@ WebFontConfig = {
 	(function() {
 	var wf = document.createElement('script');
 	wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-	  '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	  '://ajax.useso.com/ajax/libs/webfont/1/webfont.js';
 	wf.type = 'text/javascript';
 	wf.async = 'true';
 	var s = document.getElementsByTagName('script')[0];
